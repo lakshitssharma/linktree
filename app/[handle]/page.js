c7dadfe86db47d5822235e158552c202b1c66335
lakshitssharma <lakshitssharma110910@gmail.com>
2025-07-11 01:26:45 +0530

    Update page.js

--- app/[handle]/page.js
+++ app/[handle]/page.js
@@ -37,11 +37,18 @@ export default async function Page({ params }) {
         <div className="links">
   {item.links.map((linkItem, index) => {
     return (
-      <Link key={index} target='_blank' href={linkItem.link.startsWith("http") ? linkItem.link : `https://${linkItem.link}`} passHref>
-  <div className="py-4 px-2 my-3 min-w-100 bg-purple-100 rounded-md shadow-lg transition-transform duration-150 hover:scale-105 hover:shadow-xl">
-    {linkItem.linktext}
-  </div>
+     <Link
+  key={index}
+  href={linkItem.link.startsWith("http") ? linkItem.link : `https://${linkItem.link}`}
+  passHref
+>
+  <a target="_blank" rel="noopener noreferrer">
+    <div className="py-4 px-2 my-3 min-w-100 bg-purple-100 rounded-md shadow-lg transition-transform duration-150 hover:scale-105 hover:shadow-xl">
+      {linkItem.linktext}
+    </div>
+  </a>
 </Link>
+
 
     );
   })}
